84eb21edfe13ae6e7c89419e00eda62676200dcc
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2024-09-08 16:30:40 +1000

    connect to DB, import relevant npm packages to server.js

---
+++ controllers/auth.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const router = express.Router();
+const User = require('../models/user.js');
+const bcrypt = require('bcrypt')
+
+// Signup form
+router.get('/sign-up', (req, res) => {
+    res.render('auth/sign-up')
+})
+
+// Signup form submit
+router.post('/sign-up', async (req, res) => {
+    const userExists = await User.findOne( { username: req.body.username })
+
+    if( userExists !== null ){
+        res.render('auth/sign-up', { errMessage: 'There was an error logging in, did you forget your details?'});
+        return;
+    }
+
+    if( req.body.password !== req.body.passwordConfirm ){
+        res.render('auth/sign-up', { errMessage: `Your passwords don't match`});
+        return;
+    }
+
+    const hashedPassword = bcrypt.hashSync( req.body.password, 10);
+    req.body.password = hashedPassword
+
+    const newUser = await User.create({
+        username: req.body.username,
+        password: hashedPassword,
+    });
+
+    res.redirect('/')
+});
+
+// Login form
+router.get('/login', (req, res) => {
+    res.render('auth/login')
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -8,3 +8,35 @@ const morgan = require('morgan');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 
+// Load models
+const Drink = require('./models/drink');
+const User = require('./models/user');
+
+app.set('view engine', 'ejs');
+
+const authController = require('./controllers/auth')
+
+app.use( morgan('dev'))
+app.use( express.static('public'))
+app.use( express.urlencoded({ extended: true }))
+app.use( methodOverride('_method', { methods: ['POST', 'GET']}));
+
+app.use( session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI})
+}));
+
+// Connect to database
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () => {
+    console.log(`Connected!`)
+})
+
+app.use('/auth', authController)
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening at http://localhost:${process.env.PORT}`)
+});
