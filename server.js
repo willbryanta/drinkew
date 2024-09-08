3f38e050ea879b1dcf3df39b0feb667a769471fc
Will Bryant <97323893+willbryanta@users.noreply.github.com>
2024-09-08 14:12:13 +1000

    user model

---
+++ server.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env'});
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
